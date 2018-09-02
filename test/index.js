c52050644c01291b8205c918fd8f825123685f1b
jonatansalas <jonatansalas@live.com.ar>
2018-09-02 13:04:52 -0300

    Add more test cases related to Play Action

--- test/index.js
+++ test/index.js
@@ -6,41 +6,69 @@ import Constants from '../src/components/AudioPlayer/utils/constants';
 
 describe("Actions", () => {
     describe("Play", () => {
-        let mockedPlayer = createMockedPlayer({ paused: true });
+        let mockedPlayer = null;
 
-        it("When calling `playAudio` should return an Object", done => {
+        it("When calling `playAudio` should return an Object.", done => {
+            mockedPlayer = createMockedPlayer({ paused: true });
             const status = playAudio(mockedPlayer);
 
             expect(status).to.be.a("object");
             done();
         });
 
-        it("When calling `playAudio` should return an Object that has a property called `playStatus`", done => {
+        it("When calling `playAudio` should return an Object that has a property called `playStatus`.", done => {
+            mockedPlayer = createMockedPlayer({ paused: true });
             const status = playAudio(mockedPlayer);
 
             expect(status).to.have.property('playStatus');
             done();
         });
 
-        it("When calling `playAudio` should return an Object that has a property called `playStatus` which is a String", done => {
+        it("When calling `playAudio` should return an Object that has a property called `playStatus` which is a String.", done => {
+            mockedPlayer = createMockedPlayer({ paused: true });
             const status = playAudio(mockedPlayer);
 
             expect(status.playStatus).to.be.a("string");
             done();
         });
 
-        it("When calling `playAudio` and player.paused is true, should return an Object that has a property called `playStatus` with value `play`", done => {
+        it("When calling `playAudio` and player.paused value is true, should return an Object that has a property called `playStatus` with value `play`.", done => {
+            mockedPlayer = createMockedPlayer({ paused: true });
             const status = playAudio(mockedPlayer);
 
             expect(status.playStatus).to.be.equal(Constants.Status.PLAY);
             done();
         });
 
-        it("When calling `playAudio` and player.paused is false, should return an Object that has a property called `playStatus` with value `pause`", done => {
-            mockedPlayer = createMockedPlayer({ paused: false });
-            const status = playAudio(mockedPlayer);
+        it("When calling `playAudio` and player.paused value is true, player.paused should be a boolean.", done => {
+            mockedPlayer = createMockedPlayer({ paused: true });
+            playAudio(mockedPlayer);
 
-            expect(status.playStatus).to.be.equal(Constants.Status.PAUSE);
+            expect(mockedPlayer.paused).to.be.a("boolean");
+            done();
+        });
+
+        it("When calling `playAudio` and player.paused value is false, player.paused should be a boolean.", done => {
+            mockedPlayer = createMockedPlayer({ paused: false });
+            playAudio(mockedPlayer);
+
+            expect(mockedPlayer.paused).to.be.a("boolean");
+            done();
+        });
+
+        it("When calling `playAudio` and player.paused value is true, player.paused should change to false.", done => {
+            mockedPlayer = createMockedPlayer({ paused: true });
+            playAudio(mockedPlayer);
+
+            expect(mockedPlayer.paused).to.be.equal(false);
+            done();
+        });
+
+        it("When calling `playAudio` and player.paused value is false, player.paused should change to true.", done => {
+            mockedPlayer = createMockedPlayer({ paused: false });
+            playAudio(mockedPlayer);
+
+            expect(mockedPlayer.paused).to.be.equal(true);
             done();
         });
     });
--- test/utils/index.js
+++ test/utils/index.js
@@ -1,7 +1,15 @@
-export const createMockedPlayer = ({ loop = false, muted = false, paused = true }) => ({
-    loop,
-    muted,
-    paused,
-    play: () => {},
-    pause: () => {},
-});
+export const createMockedPlayer = ({ loop = false, muted = false, paused = true }) => {
+    const player = {
+        loop,
+        muted,
+        paused,
+        play: () => {
+            player.paused = false;
+        },
+        pause: () => {
+            player.paused = true;
+        },
+    };
+
+    return player;
+};
